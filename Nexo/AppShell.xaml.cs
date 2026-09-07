54bcf917216629ec405c3d8316b8007de1810bb0
RENAN <renzogeek@gmail.com>
2026-09-07 16:41:06 -0300

    Adicionar arquivos de projeto.

---
+++ Nexo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Nexo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nexo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Nexo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Nexo.Pages")]
